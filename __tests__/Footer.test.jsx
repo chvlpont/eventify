7564de377228904350170645a7b79e8dc9f4d380
cvhlpont <pontchvl@gmail.com>
2024-05-31 19:24:10 +0200

    update: implemented tests for footer

---
+++ __tests__/Footer.test.jsx
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Footer from "@/components/Footer";
+
+describe("Footer", () => {
+  it("should render the footer containing a div with 'Contact Us'", () => {
+    render(<Footer />); // ARRANGE
+    const divElement = screen.getByText("Contact Us"); // ACT
+    expect(divElement).toBeInTheDocument(); // ASSERT
+  });
+
+  it("should render the footer containing a div with 'About'", () => {
+    render(<Footer />); // ARRANGE
+    const divElement = screen.getByText("About"); // ACT
+    expect(divElement).toBeInTheDocument();
+  });
+
+  it("should render the footer containing a div with 'FAQ'", () => {
+    render(<Footer />); // ARRANGE
+    const divElement = screen.getByText("FAQ"); // ACT
+    expect(divElement).toBeInTheDocument(); // ASSERT
+  });
+});
